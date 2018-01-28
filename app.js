3083cdef7603d54aad63e08f79ec990f2fa9131c
fenghengzhi <fenghengzhi@gmail.com>
2018-01-28 21:21:53 +0800

    new

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const fetch = require('node-fetch');
-const cheerio = require('cheerio');
 const fs = require('fs');
-const config = JSON.parse(fs.readFileSync('config.json'))
-fetch('http://ddns.oray.com/checkip')
-    .then(res => res.text())
-    .then((d) => {
-        const $ = cheerio.load(d);
-        const body = $('body').text();
-        const ip = body.replace('Current IP Address: ', '');
-        return fetch(`http://username:password@ddns.oray.com/ph/update?hostname=yourhostname&myip=ipaddress`);
-    });
+const config = JSON.parse(fs.readFileSync('config.json'));
+const username = config.username;
+const password = config.password;
+const hostname = config.hostname;
+
+function updateIp() {
+    fetch('http://ddns.oray.com/checkip')
+        .then(res => res.text())
+        .then((d) => {
+            const ip = d.match(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/)[0];
+            return fetch(`http://${username}:${password}@ddns.oray.com/ph/update?hostname=${hostname}&myip=${ip}`);
+        });
+}
+
+setInterval(updateIp, 1000 * 60 * 60);
