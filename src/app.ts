40243359ee3870f3746e07fe021b74d835681a30
fenghengzhi <fenghengzhi@gmail.com>
2018-06-03 19:29:35 +0800

    edit

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,9 @@ function updateIp() {
         .then(html => cheerio.load(html))
         .then($ => $('body').text())
         // .then(d => console.log(d))
-        .then(d => {
-            const ip = d.match(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/)[0];
+        .then(body => {
+            const ip = body.match(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/)[0];
+            // console.log(ip, `http://${username}:${password}@ddns.oray.com/ph/update?hostname=${hostname}&myip=${ip}`)
             return Axios.get(`http://${username}:${password}@ddns.oray.com/ph/update?hostname=${hostname}&myip=${ip}`);
         });
 }
